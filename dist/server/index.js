67847ffad8f2971889536af82dffad92640c1050
Kazuhiro-Tokumoto <20250148@st.shudo-h.ed.jp>
2026-03-18 17:36:45 +0900

    はおぁ　つかれた

--- dist/server/index.js
+++ dist/server/index.js
@@ -1,6 +1,6 @@
 import { ecchimailserverAPI } from "./api.js";
 try {
-    new ecchimailserverAPI(5000, "ecchi.manh2309.org", "localhost");
+    new ecchimailserverAPI(4000, "ecchi.manh2309.org", "localhost");
     console.log("起動した");
 }
 catch (e) {
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,7 @@
 import { ecchimailserverAPI } from "./api.js";
 
 try {
-    new ecchimailserverAPI(5000, "ecchi.manh2309.org", "localhost");
+    new ecchimailserverAPI(4000, "ecchi.manh2309.org", "localhost");
     console.log("起動した");
 } catch (e) {
     console.error("エラー:", e);
